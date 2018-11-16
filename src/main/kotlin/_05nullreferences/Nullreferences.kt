afab584721ddc0ec3f7e310f12bcac1c1af0435c
Dima Koltovich <steklopod@gmail.com>
2018-11-16 17:23:39 +0300

    Kotlin nullability example refactored.

--- src/main/kotlin/_05nullreferences/Nullreferences.kt
+++ src/main/kotlin/_05nullreferences/Nullreferences.kt
@@ -2,36 +2,28 @@ package _05nullreferences
 
 fun main(args: Array<String>) {
 
-    // nullable with ?
     val strOne: String? = null
 
     val strTwo: String? = "Nullable, but not null"
-    // now the compiler complains if we do
-    // strTwo.toUpperCase()
-    // smart casting to the rescue
-    if (strTwo != null) {
-        strTwo.toUpperCase()
+    // плохая практика   :-(
+    if (strTwo != null) strTwo.toUpperCase()
 
-    }
-
-    // but even better, we can use the safe access expression
+    // хоршая практика   :-)
     strTwo?.toUpperCase()
     println("Let's see what happens here: ${strOne?.toUpperCase()}")
+
 
     // null checks
     val country = Country(3)
     val address = Address(country)
     val branch = Branch(address)
 
-    val code1 = country.code
-    // really easy nested null checks
     val code: Int? = branch?.address?.country?.code
-    println(code)
+    println("code without `null`: $code")
 
-    // or even chain null checks and provide default value
-    val branch2: Branch = Branch(null)
-    val code2: Int? = branch2?.address?.country?.code ?: 599
-    println(code2)
+    val code2: Int? = Branch(null)?.address?.country?.code ?: 599
+    println("code with default, cause `null`: $code2")
+
 
     // elvis operator
     val nullString: String? = null
@@ -40,7 +32,7 @@ fun main(args: Array<String>) {
     // safe cast operator, datatype has to be nullable (: String?)
     val something: Any = arrayOf(1, 2, 3, 4)
     val someString = something as? String
-    println(someString)
+    println("something as? String : $someString")
 
     // non null assertion with !! If str is indeed null, 'NullPointerException' is thrown
     val noNullString: String? = "This is definitely noy null"
@@ -80,9 +72,7 @@ fun main(args: Array<String>) {
 }
 
 class Branch(val address: Address?)
-
 class Address(val country: Country)
-
 class Country(val code: Int)
 
 fun printText(text: String) {
